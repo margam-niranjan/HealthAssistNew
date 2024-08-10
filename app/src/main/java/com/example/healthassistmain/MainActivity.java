232fd2bb3cb96a9c57bd3da42cd52597dd02ed16
Niranjan Margam <niranjanchintuwin2004@gmail.com>
2024-08-10 22:51:01 +0530

    completed starting splash screen with logos and searchBar

---
+++ app/src/main/java/com/example/healthassistmain/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.healthassistmain;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, HomePage.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
