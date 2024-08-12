6ef650dbc03b69193ccaa169296c1fe8aa7bd95c
Niranjan Margam <niranjanchintuwin2004@gmail.com>
2024-08-12 22:17:37 +0530

    added event listeners for profile onclick

--- app/src/main/java/com/example/healthassistmain/HomePage.java
+++ app/src/main/java/com/example/healthassistmain/HomePage.java
@@ -1,14 +1,21 @@
 package com.example.healthassistmain;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class HomePage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,29 @@ public class HomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//        onclick for navigation bar
+        Toolbar toolbar = findViewById(R.id.menuToolBar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomePage.this, "Toast got it", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        Onclick for  profile on the top
+        View profile = findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+                                       public void onClick(View v){
+                                           Toast.makeText(HomePage.this, "profile Clicked", Toast.LENGTH_SHORT).show();
+                                       }
+                                   });
+
+
+
     }
+
+
+
 
 }
