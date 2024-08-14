786924e38056edd52ab0a8aa6aa68b04a86a48ac
Niranjan Margam <niranjanchintuwin2004@gmail.com>
2024-08-14 22:15:26 +0530

    Added navigation drawer to home page, loaded vector assets, changed some values of colors

--- app/src/main/java/com/example/healthassistmain/HomePage.java
+++ app/src/main/java/com/example/healthassistmain/HomePage.java
@@ -1,20 +1,30 @@
 package com.example.healthassistmain;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePage extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
 
     @Override
@@ -28,22 +38,66 @@ public class HomePage extends AppCompatActivity {
             return insets;
         });
 
-//        onclick for navigation bar
-        Toolbar toolbar = findViewById(R.id.menuToolBar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(HomePage.this, "Toast got it", Toast.LENGTH_SHORT).show();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if(id == R.id.nav_profile){
+                    Toast.makeText(HomePage.this, "Home", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_medical_info){
+                    Toast.makeText(HomePage.this, "Medical Info", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_bmi){
+                    Toast.makeText(HomePage.this, "BMI", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_medicines){
+                    Toast.makeText(HomePage.this, "Book Medicines", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_consult){
+                    Toast.makeText(HomePage.this, "Consult a doctor", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_check_appointment){
+                    Toast.makeText(HomePage.this, "Check your appointments", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_book){
+                    Toast.makeText(HomePage.this, "book your appointments", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_pay){
+                    Toast.makeText(HomePage.this, "Pay bills", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_exercise){
+                    Toast.makeText(HomePage.this, "Exercise", Toast.LENGTH_SHORT).show();
+                }
+
+                if(id == R.id.nav_diet){
+                    Toast.makeText(HomePage.this, "Diet", Toast.LENGTH_SHORT).show();
+                }
+
+
+                return true;
             }
         });
-
-//        Onclick for  profile on the top
-        View profile = findViewById(R.id.profile);
-        profile.setOnClickListener(new View.OnClickListener() {
-                                       public void onClick(View v){
-                                           Toast.makeText(HomePage.this, "profile Clicked", Toast.LENGTH_SHORT).show();
-                                       }
-                                   });
 
 
 
